f9a7dca164932d709fb5e9d62355b1f5c7335ea3
linusg9 <linusg910@gmail.com>
2026-04-29 01:10:44 +0530

    Refactor LCG.java for formatting consistency

--- LCG.java
+++ LCG.java
@@ -1,34 +1,26 @@
 import java.util.*;
-
-public class LCG {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter seed (X0): ");
-        int seed = sc.nextInt();
-
-        System.out.print("Enter multiplier (a): ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter increment (c): ");
-        int c = sc.nextInt();
-
-        System.out.print("Enter modulus (m): ");
-        int m = sc.nextInt();
-
-        System.out.print("Enter number of random numbers to generate: ");
-        int n = sc.nextInt();
-
-        int X = seed;
-
-        System.out.println("\nGenerated Pseudo Random Numbers:");
-
-        for (int i = 0; i < n; i++) {
-            X = (a * X + c) % m;
-            System.out.println("X" + (i + 1) + " = " + X);
-        }
-
-        sc.close();
-    }
+public class LCG
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter seed (X0): ");
+int seed=sc.nextInt();
+System.out.print("Enter multiplier (a): ");
+int a=sc.nextInt();
+System.out.print("Enter increment (c): ");
+int c=sc.nextInt();
+System.out.print("Enter modulus (m): ");
+int m=sc.nextInt();
+System.out.print("Enter number of random numbers to generate: ");
+int n=sc.nextInt();
+int X=seed;
+System.out.println("\nGenerated Pseudo Random Numbers:");
+for(int i=0;i<n;i++)
+{
+X=(a*X+c)%m;
+System.out.println("X"+(i+1)+" = "+X);
+}
+sc.close();
+}
 }
